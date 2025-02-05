5cd16d7ab71d01cc599b4cbd008055f132f6c51d
aesaraesaraesar <aesar12345a@gmail.com>
2025-02-05 10:23:06 +0300

    Update events.py

--- JoKeRUB/core/events.py
+++ JoKeRUB/core/events.py
@@ -67,7 +67,7 @@ class NewMessage(events.NewMessage):
                 event._client.loop.create_task(edit_or_reply(event, text))
                 return
         if gvarstatus("blockedfrom") == "yes":
-            event._client.loop.create_task(edit_or_reply(event, "**᯽︙ لا يمكنك أستخدام سورس 7rB  لأنك محظور من قبل المطور**"))
+            event._client.loop.create_task(edit_or_reply(event, "**᯽︙ لا يمكنك أستخدام سورس HELLAS  لأنك محظور من قبل المطور**"))
             return
         return event
 
